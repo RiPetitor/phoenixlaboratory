f9061601b962721d376faddfb8504c2af0283be6
Sergey Braumann <ripetitor-645@ya.ru>
2025-06-06 16:09:16 +0500

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/fawicon.ico',
 
   // Set the production url of your site here
-  url: 'https://phoenixlab.book',
+  url: 'https://phoenixlab.pages.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
