5bd5381820df4fe995c4f687e074eb1ad2cd83f0
Sergey Braumann <ripetitor-645@ya.ru>
2025-08-16 18:50:30 +0500

    Добавление проекта на Гит

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,164 +4,155 @@
 // Существуют различные эквивалентные способы объявления конфигурации вашего Docusaurus.
 // Смотрите: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 // Это выполняется в Node.js - Здесь не используется клиентский код (API браузера, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Phoenix Labaratory',
-  tagline: 'Место, для изучения чего-то нового!',
+  title: "Phoenix Labaratory",
+  tagline: "Место, для изучения чего-то нового!",
   //Иконка сайта
-  favicon: 'img/fawicon.ico',
+  favicon: "img/fawicon.ico",
 
   // Укажите рабочий URL вашего сайта здесь
-  url: 'https://phoenixlab.pages.dev',
+  url: "https://phoenixlab.pages.dev",
   // Укажите /<baseUrl>/ путь, по которому будет доступен ваш сайт
   // Для развертывания страниц на GitHub часто используется "/<Имя проекта>/"
-  baseUrl: '/',
+  baseUrl: "/",
 
-// Конфигурация развертывания страниц GitHub.
-// Если вы не используете страницы GitHub, они вам не нужны.
-  organizationName: 'phoenixlab', // Обычно это ваша организация на GitHub/имя пользователя.
-  projectName: 'LearnBook', // Обычно это имя вашего репозитория.
+  // Конфигурация развертывания страниц GitHub.
+  // Если вы не используете страницы GitHub, они вам не нужны.
+  organizationName: "phoenixlab", // Обычно это ваша организация на GitHub/имя пользователя.
+  projectName: "LearnBook", // Обычно это имя вашего репозитория.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
-// Даже если вы не используете интернационализацию, вы можете использовать это поле для задания
-// полезных метаданных, таких как язык html. Например, если ваш сайт на китайском, вы
-// можете захотеть заменить "en" на "zh-Hans".
+  // Даже если вы не используете интернационализацию, вы можете использовать это поле для задания
+  // полезных метаданных, таких как язык html. Например, если ваш сайт на китайском, вы
+  // можете захотеть заменить "en" на "zh-Hans".
   i18n: {
-    defaultLocale: 'ru',
-    locales: ['ru', 'en'],
+    defaultLocale: "ru",
+    locales: ["ru", "en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Пожалуйста, измените это на свой репозиторий.
           // Удалите это, чтобы удалить ссылки "редактировать эту страницу".
-          editUrl:
-            'https://gitlab.com/phoenixlab2/project-phoenixlab',
+          editUrl: "https://gitverse.ru/Phoenix97/PhoenixLab-site",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Пожалуйста, измените это на свой репозиторий.
           // Удалите это, чтобы удалить ссылки "редактировать эту страницу"
-          editUrl:
-            'https://gitlab.com/phoenixlab2/project-phoenixlab',
+          editUrl: "https://gitverse.ru/Phoenix97/PhoenixLab-site",
           // Полезные опции для внедрения лучших практик ведения блога
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
   ],
 
-  
-
   themeConfig:
-    /**/// @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
+    /**/ // @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    {
       // Настройка стартовой темы
-      colorMode:{
-        defaultMode: 'dark',
+      colorMode: {
+        defaultMode: "dark",
         disableSwitch: false,
         respectPrefersColorScheme: false,
       },
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'PhoenixLab',
+        title: "PhoenixLab",
         logo: {
-          alt: 'Мой сайт',
-          src: 'img/logo.svg',
+          alt: "Мой сайт",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'docsSidebar',
-            position: 'left',
-            label: 'Документация',
+            type: "docSidebar",
+            sidebarId: "docsSidebar",
+            position: "left",
+            label: "Документация",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'learnSidebar',
-            position: 'left',
-            label: 'Учебник',
+            type: "docSidebar",
+            sidebarId: "learnSidebar",
+            position: "left",
+            label: "Учебник",
           },
-          {to: '/blog', label: 'Блог', position: 'left'}, // Блог
-          {type: 'localeDropdown', position: 'right'}, // Кнопка с языками
+          { to: "/blog", label: "Блог", position: "left" }, // Блог
+          { type: "localeDropdown", position: "right" }, // Кнопка с языками
           {
-            href: 'https://gitlab.com/phoenixlab2/project-phoenixlab',
-            label: 'GitLab',
-            position: 'right',
+            href: "https://gitverse.ru/Phoenix97/PhoenixLab-site",
+            label: "GitVerse",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Главное',
+            title: "Главное",
             items: [
               {
-                label: 'Документация',
-                to: '/docs/doces/intro',
+                label: "Документация",
+                to: "/docs/doces/intro",
+              },
+              {
+                label: "Учебник",
+                to: "/docs/learn/learn-intro",
+              },
+              {
+                label: "Блог",
+                to: "/blog",
               },
             ],
           },
           {
-            title: 'Участники',
+            title: "Следите за нами!",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "ВКонтакте",
+                href: "https://vk.com/ntscphoenix",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "Telegram",
+                href: "https://t.me/myphoenixlife",
               },
               {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'Следите за нами!',
-            items: [
-              {
-                label: 'Блог',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitVerse",
+                href: "https://gitverse.ru/Phoenix97/PhoenixLab-site",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Открытый проект, созданный с помощью Docusaurus!`,
+        copyright: `Copyright © ${new Date().getFullYear()} PhoenixLab. Создано с ❤️`,
       },
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
-    }),
+    },
 };
 
 export default config;
